858839c297c7c31b6ade417e4433bcbb57d4d35a
Edison Neto <ednetoali@gmail.com>
2018-04-18 17:47:41 -0300

    Changed everything to a package

---
+++ hek-active-region/__init__.py
@@ -0,0 +1,1 @@
+from .hek import HEK
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="hek-active-region",
+    version="0.1",
+    author="Edison Neto",
+    author_email="ednetoali@gmail.com",
+    packages=["hek-active-region"],
+    include_package_data=True,
+    install_requires=[
+        "requests"
+    ],
+)
